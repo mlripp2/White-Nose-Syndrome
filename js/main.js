cd2981c8b64017d6756335c5df9c865ff301da4f
mlripp2 <mlripp2@wisc.edu>
2014-12-01 20:53:01 -0600

    syntax errors

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 window.onload =initialize();
 
+function initialize(){
+	setMap();
+};
+
 
 function setMap(){
-	var width = 600;
-	var height = 300;
+	//var width = 600;
+	//var height = 300;
 
-	var map = d3.select("body")
+/*	var map = d3.select("body")
 		.attr("width",width)
 		.attr("height",height)
 		.attr("class", "map");
@@ -15,18 +19,26 @@ function setMap(){
 		.translate([width / 2, height / 2]);
 	
 	var path = d3.geo.path()
-		.projection(projection);
+		.projection(projection);*/
 	
-	
+
+/**
+ * Fetches the JSON object and loads it asynchronously for use.
+ * Queue waits until the json files are completely loaded and then calls 
+ * the function in await to start everything else
+ */
+function getdata(){	
 	queue()
 		.defer(d3.json, "data/WNS_County.topojson")
 		.defer(d3.json, "data/NorthAmerica.topojson")
 		.await(callback);
 	}
 
-function callback (WNS_County, NorthAmerica)
+function callback (error, WNS_County, NorthAmerica){
+	console.log(NorthAmerica);
+	}
 
-	var county = map.selectAll(".county") //create SVG path element
+/*	var county = map.selectAll(".county") //create SVG path element
         .data(topojson.feature(WNS_County, WNS_County.type.objects.collection)
         .enter() //create elements
 		.append("g") //give each province its own g element 
@@ -40,4 +52,4 @@ function callback (WNS_County, NorthAmerica)
        
        	createDropdown(csvData); //create the dropdown menu
 		setChart(csvData, colorize); //create the bar chart
-	};
+	};*/
